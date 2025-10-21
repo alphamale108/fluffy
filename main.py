5ce8cadee99a9de3d48430f8bfe59c7592344e9d
alphamale108 <136615536+alphamale108@users.noreply.github.com>
2025-10-21 18:23:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -430,8 +430,17 @@ async def txt_handler(bot: Client, m: Message):
             
             if "utkarshapp" and ".pdf"  in url:
                 #It is pdf,so download pdf file
-                cmd = f'yt-dlp -o "{name}.pdf" "{url}"'
-                download_cmd = f"{cmd} -R 25 --fragment-retries 25"
+                        try:
+                            cmd = f'yt-dlp -o "{name}.pdf" "{url}"'
+                            download_cmd = f"{cmd} -R 25 --fragment-retries 25"
+                            os.system(download_cmd)
+                            copy = await bot.send_document(chat_id=m.chat.id, document=f'{name}.pdf', caption=cc1)
+                            count += 1
+                            os.remove(f'{name}.pdf')
+                        except FloodWait as e:
+                            await m.reply_text(str(e))
+                            time.sleep(e.x)
+                            continue 
 
 
             elif "utkarshapp" in url:
