50176d9af3cdab6b9ea908e478c0719853690c34
alphamale108 <136615536+alphamale108@users.noreply.github.com>
2025-10-21 10:22:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -435,8 +435,8 @@ async def txt_handler(bot: Client, m: Message):
             #elif "youtube.com" in url or "youtu.be" in url:
                 #cmd = f'yt-dlp --cookies youtube_cookies.txt -f "{ytf}" "{url}" -o "{name}".mp4'
 
-            elif:
-                cmd = f'yt-dlp --add-header "Referer: https://player.akamai.net.in" -f "{ytf}" "{url}" -o "{name}.mp4"'
+          """  elif:
+                cmd = f'yt-dlp --add-header "Referer: https://player.akamai.net.in" -f "{ytf}" "{url}" -o "{name}.mp4"' """
 
             else:
                 
