c35ff11418704c7ac48a382e6d632941e8acc1cf
alphamale108 <136615536+alphamale108@users.noreply.github.com>
2025-12-16 20:17:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -550,9 +550,9 @@ async def txt_handler(bot: Client, m: Message):
                         await m.reply_text(str(e))
                         time.sleep(e.x)
                         count += 1
-                        continue"""
+                        continue
 
-               elif ".pdf" in url:
+               """elif ".pdf" in url:
                     try:
                         cmd = f'yt-dlp -o "{name}.pdf" --add-header "Referer: https://player.akamai.net.in" "{url}"'
                         download_cmd = f"{cmd} -R 25 --fragment-retries 25"
